604d21f657959e498d01309fd82941c70d908ae2
John Bariquit <john@Wifi-Test.ripeconcepts.net>
2015-05-13 14:23:27 +0800

    Initial Commit

---
+++ NuvitaCardio Pro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NuvitaCardio Pro
+//
+//  Created by John Bariquit on 5/13/15.
+//  Copyright (c) 2015 John Bariquit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NuvitaCardio Pro/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  NuvitaCardio Pro
+//
+//  Created by John Bariquit on 5/13/15.
+//  Copyright (c) 2015 John Bariquit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
